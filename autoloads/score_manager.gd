28a11f6ca863533e7768f11c96984472219b08d1
Nils Nahooy <nils.nahooy@live.com>
2024-07-26 19:42:35 +0200

    save score to disk

--- autoloads/score_manager.gd
+++ autoloads/score_manager.gd
@@ -1,9 +1,12 @@
 extends Node
 
 const DEFAULT_SCORE: int = 1000
-const SCORE_FILE: String = "user://animals.dat"
+const SCORE_FILE: String = "user://animals.json"
 
 var _level_scores: Dictionary = {}
+
+func _ready() -> void:
+	load_from_disk()
 
 func _check_and_add_level(level: String) -> void:
 	if !_level_scores.has(level):
@@ -13,7 +16,21 @@ func set_score_for_level(score: int, level: String) -> void:
 	_check_and_add_level(level)
 	if score < _level_scores[level]:
 		_level_scores[level] = score
+		save_to_disk()
 
 func get_level_score(level: String) -> int:
 	_check_and_add_level(level)
 	return _level_scores[level]
+
+func save_to_disk() -> void:
+	var file: FileAccess = FileAccess.open(SCORE_FILE, FileAccess.WRITE)
+	var score_json: String = JSON.stringify(_level_scores)
+	file.store_string(score_json)
+
+func load_from_disk() -> void:
+	var file: FileAccess = FileAccess.open(SCORE_FILE, FileAccess.READ)
+	if file == null:
+		save_to_disk()
+	else:
+		var score_json: String = file.get_as_text()
+		_level_scores = JSON.parse_string(score_json)
