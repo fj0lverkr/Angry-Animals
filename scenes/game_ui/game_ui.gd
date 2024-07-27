87049c6001a3b7da9b1933ac558bc428295244e1
Nils Nahooy <nils.nahooy@live.com>
2024-07-27 10:56:48 +0200

    create level complete scene

---
+++ scenes/game_ui/game_ui.gd
@@ -0,0 +1,33 @@
+extends Control
+
+@onready var bgm: AudioStreamPlayer = $"BGM"
+@onready var level_label: Label = $"MarginContainer/VBTopLeft/LevelLabel"
+@onready var attempts_label: Label = $"MarginContainer/VBTopLeft/AttemptsLabel"
+@onready var vb_center: VBoxContainer = $"MarginContainer/VBCenter"
+
+var level_completed: bool
+var attempts: int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	vb_center.hide()
+	mouse_filter = Control.MOUSE_FILTER_PASS
+	bgm.seek(SceneManager.bgm_position)
+	level_completed = false
+	level_label.text = "Level: %s" % SceneManager.get_selected_level()
+	SignalBus.on_level_completed.connect(_on_level_completed)
+	SignalBus.on_score_updated.connect(_on_score_updated)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("menu") and level_completed:
+		SceneManager.bgm_position = bgm.get_playback_position()
+		get_tree().change_scene_to_packed(SceneManager.MAIN)
+
+func _on_score_updated(score: int) -> void:
+	attempts_label.text = "Attempts: %s" % score
+
+func _on_level_completed() -> void:
+	vb_center.show()
+	mouse_filter = Control.MOUSE_FILTER_STOP
+	level_completed = true
--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -1,21 +1,11 @@
 extends Node2D
 
 @onready var spawn_marker: Marker2D = $"AnimalSpawn"
-@onready var bgm: AudioStreamPlayer = $"BGM"
-@onready var attempt_label: Label = $Control/MarginContainer/AttemptsLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	bgm.seek(SceneManager.bgm_position)
 	SignalBus.on_parrot_died.connect(_on_water_hit)
-	SignalBus.on_score_updated.connect(_on_score_updated)
-	SignalBus.on_level_completed.connect(_on_level_completed)
 	spawn_parrot()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if Input.is_key_pressed(KEY_ESCAPE):
-		_go_to_main_scene()
 
 func spawn_parrot() -> void:
 	var parrot_instance: Node = SceneManager.PARROT.instantiate()
@@ -24,13 +14,3 @@ func spawn_parrot() -> void:
 
 func _on_water_hit() -> void:
 	call_deferred("spawn_parrot")
-
-func _on_score_updated(score: int) -> void:
-	attempt_label.text = str(score)
-
-func _go_to_main_scene() -> void:
-	SceneManager.bgm_position = bgm.get_playback_position()
-	get_tree().change_scene_to_packed(SceneManager.MAIN)
-
-func _on_level_completed() -> void:
-	_go_to_main_scene()
